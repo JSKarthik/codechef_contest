cc89a916aa17944e32e0b1b4ab5be80d1b00e305
Karthikeyan J <78247442+JSKarthik@users.noreply.github.com>
2022-02-05 17:50:33 +0530

    Update add_two_num.c

--- add_two_num.c
+++ add_two_num.c
@@ -1,14 +1,16 @@
+/*Shivam is the youngest programmer in the world, he is just 12 years old. Shivam is learning programming and today he is writing his first program.
+The task is very simple: given two integers A and B, write a program to add these two numbers and output it.*/
 #include <stdio.h> 
-
 int main() 
 {
   int T;
-	scanf("%d", &T);
-	 while (T--) {
-	  int a, b;
-		scanf("%d %d", &a, &b);
-    int ans = a + b;
-		printf("%d\n", ans);
-	}
+  scanf("%d", &T);
+  while (T--) 
+     {
+  	int a, b;
+	scanf("%d %d", &a, &b);
+        int ans = a + b;
+        printf("%d\n", ans);
+     }
   return 0;
 }
